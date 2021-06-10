a6c04e94b99cb4dcc64b3910d3206ebce7579526
DPCaraballe <84527568+DPCaraballe@users.noreply.github.com>
2021-06-10 13:59:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('Hallo Winc Academy studenten');
+
+// dit is een grote som 
+let multiplication = 230941 * 1823794;
+console.log('multiplication', multiplication);
+let division = 637263 / 54;
+console.log('division', division);
+
+/* 
+let multiplication = 230941 * 1823794;
+console.log('multiplication', multiplication)
+let division = 637263 / 54;
+console.log('division', division)
+*/
